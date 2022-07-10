3f3c7697d7c7d2da7e63a8df545fb105e61e1421
MarcosG85 <gavinamarcos@gmail.com>
2022-07-10 12:00:02 -0300

    Integramos todos los cambios htmls y estilos

--- Home/app.js
+++ Home/app.js
@@ -11,3 +11,14 @@ app.get('/', (req,res)=>{
     res.sendFile(__dirname + '/views/home.html');
 });
 
+app.get('/Cart', (req,res)=>{
+    res.sendFile(__dirname + '/views/Cart.html');
+});
+
+app.get('/login', (req,res)=>{
+    res.sendFile(__dirname + '/views/login.html');
+});
+
+app.get('/register', (req,res)=>{
+    res.sendFile(__dirname + '/views/register.html');
+});
