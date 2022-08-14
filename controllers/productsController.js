49d2faac16dc9e7cf869905f1bf0419962112c3e
MarianaElsne <elstnermariana@gmail.com>
2022-08-14 12:39:42 -0300

    agragamos fs, path, json, thousand, agregamos la logica de products,  render de datail, products create a ingles,  agregamos el methodo store

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,18 +1,39 @@
 const express = require('express');
-const path = require('path');
-const productsBaseDatos = require('../dataBase/productsDataBase.json');
-const { products } = require('./mainControllers');
 
-//** empece la logica de controllers Todavia no linkaeas con la pagina products*/
+/**fs para llamar la funcion y escribir un archivo*/
+const fs = require('fs');
+/** metodo para manejar las rutas relativas y absolutas */
+const path = require('path');
+
+/**
+ * editar archivos del json
+ */
+const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
+const product = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+
+const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+
+/** llamamos a la base de datos de productos */
+const productsBaseDatos = require('../data/productsDataBase.json');
+const mainControllers = require('./mainControllers');
+
+
+
+//** empece la logica de controllers faltan logicas*/
 const productsController = {
     products: (req, res) =>{
-        res.render('products')
+        res.render('products', {
+			product,
+			toThousand
+		})
     },
-    //mostrarPorId:(req, res) =>{
+    
+    mostrarPorId:(req, res) =>{
+    req.render('detail')
+    },
 
-   // },
-    crearProducto: (req, res) =>{
-        res.render('productsCrear');
+    createProducts: (req, res) =>{
+        res.render('productsCreate');
         
         // let nuevoProducto= {
         // Nombre_del_producto: req.body.Nombre_del_producto,
@@ -23,11 +44,30 @@ const productsController = {
         // Precio: req.body.Precio, 
         // Destacado: req.body.Destacado 
     },
-}       
+     
        // res.send(),
         //res.redirect('/'),
     //},
     
+    
+	//* Create -  Method to store/
+	store: (req, res) => {
+		let image
+		console.log(req.files);
+		if(req.files[0] != undefined){
+			image = req.files[0].filename
+		} else {
+			image = 'default-image.png'
+		}
+		let newProduct = {
+			id: products[products.length - 1].id + 1,
+			...req.body,
+			image: image
+		};
+		products.push(newProduct)
+		fs.writeFileSync(productsFilePath, JSON.stringify(products, null, ' '));
+		res.redirect('/');
+	},
     // edit: (req, res) => {
 	// 	res.render('products-edit-form');
     //     res.redirect('/');
@@ -37,6 +77,6 @@ const productsController = {
     //     res.render('products-delete')
     //     res.redirect('/');/**a donde redireccionaria despues de hacer a accion */
     // }}
-
+}
 
 module.exports = productsController;
